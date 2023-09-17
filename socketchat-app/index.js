977a8c4850120285ae87c045aae9c0aa073159f1
Georgesproat <georgesproat1@gmail.com>
2023-09-18 11:58:07 +1200

    change

--- socketchat-app/index.js
+++ socketchat-app/index.js
@@ -3,7 +3,7 @@ const app = express();
 const http = require("http");
 const { Server } = require("socket.io");
 const server = http.createServer(app);
-const io = new Server(server);
+ const io = new Server(server);
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
